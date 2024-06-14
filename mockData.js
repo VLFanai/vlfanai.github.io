e57dd34ae1aed2877bb6630fdf4ce316ce5a383b
VLFanai <89696427+VLFanai@users.noreply.github.com>
2024-06-15 00:05:06 +0530

    Add files via upload

---
+++ mockData.js
@@ -0,0 +1,32 @@
+const slides = [
+    {
+      url: 'https://images.unsplash.com/34/BA1yLjNnQCI1yisIZGEi_2013-07-16_1922_IMG_9873.jpg?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Elevating myPotrait Drawing Skills T=through AI-Generated Art',
+      description: 'Unlocking my artistic potential, I harness AI-generated art to enhance my potrait drawing skills and explere boundless creativity',
+      link: 'https://vl-fanai.blogspot.com/2023/06/elevating-my-portrait-drawing-skills.html',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Title2',
+      description: '',
+      link: '',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Title3',
+      description: '',
+      link: '',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1501785888041-af3ef285b470?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Title4',
+      description: '',
+      link: '',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1429704658776-3d38c9990511?q=80&w=1979&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Title5',
+      description: '',
+      link: '',
+    },
+  ];
